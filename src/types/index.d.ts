11387dab8f3b361a55aa37f99205b3dd590c9199
Daniela C. Barbosa <contato@danielacb.com>
2023-03-01 23:13:10 -0300

    add project types

---
+++ src/types/index.d.ts
@@ -0,0 +1,70 @@
+interface AssetProps {
+  assignedUserIds: number[];
+  companyId: number;
+  healthHistory: HealthHistory[];
+  healthscore: number;
+  id: number;
+  image: string;
+  metrics: {
+    lastUptimeAt: string;
+    totalCollectsUptime: number;
+    totalUptime: number;
+  };
+  model: string;
+  name: string;
+  sensors: string[];
+  specifications: {
+    maxTemp?: number;
+    power?: number;
+    rpm?: number;
+  };
+  status: AssetStatusProps;
+  unitId: number;
+}
+
+interface WorkordersProps {
+  assetId: number;
+  assignedUserIds: number[];
+  checklist: [
+    {
+      completed: boolean;
+      task: string;
+    }
+  ];
+  description: string;
+  id: number;
+  priority: "high" | "medium" | "low";
+  status: "in progress" | "completed" | "not started";
+  title: string;
+}
+
+interface HealthHistoryProps {
+  status: string;
+  timestamp: string;
+}
+
+interface UserProps {
+  companyId: number;
+  email: string;
+  id: number;
+  name: string;
+  unitId: number;
+}
+
+interface UnitProps {
+  companyId: number;
+  id: number;
+  name: string;
+}
+
+interface CompanyProps {
+  companyId: number;
+  id: number;
+  name: string;
+}
+
+type AssetStatusProps =
+  | "inAlert"
+  | "inOperation"
+  | "inDowntime"
+  | "unplannedStop";
